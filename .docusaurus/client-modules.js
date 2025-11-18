98946fbc0d808c5224ff4916f46723bfdf413898
Stergios Bachoumas <stevbach@udel.edu>
2025-11-18 09:50:22 -0500

    Added docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/horclab/Documents/Stergios/stergiosba.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/horclab/Documents/Stergios/stergiosba.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/horclab/Documents/Stergios/stergiosba.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/horclab/Documents/Stergios/stergiosba.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/horclab/Documents/Stergios/stergiosba.github.io/src/css/custom.css"),
+];
